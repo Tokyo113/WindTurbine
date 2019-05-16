8888d465797dc8807e89e3c106f77a80b5c46599
Tokyo113 <21810179@zju.edu.cn>
2019-05-16 21:25:24 +0800

    state

--- tk_05_rawData.py
+++ tk_05_rawData.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 import pandas as pd
 import numpy as np
 
@@ -5,13 +6,17 @@ import numpy as np
 data1 = pd.read_csv('./data/raw_data/3.csv', header=None)
 data2 = pd.read_csv('./data/raw_data/5.csv', header=None)
 data3 = pd.read_csv('./data/raw_data/6.csv', header=None)
+# 引入风机当前状态
+data4 = pd.read_csv('./data/raw_data/1.csv', header=None)
 
 data1[6] = data2[3]
 data1[7] = data3[3]
+data1[8] = data4[3]
 data1.columns = ["date", "Generator_speed", "Rotor_speed", "Gearbox_oil_temperature",
                  "Generator_bearing_temperature_drive", "Generator_bearing_temperature_nondrive",
-                 "wind_speed", "active_power"]
-print(data1.describe())
-# data1.to_csv("./data/raw_data/raw_data1.csv", index=None)
+                 "wind_speed", "active_power", "state"]
+print(data1.groupby("state").count())
+
+# data1.to_csv("./data/raw_data/raw_data_state.csv", index=None)
 
 
--- tk_07_single_month.py
+++ tk_07_single_month.py
@@ -4,10 +4,11 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("./data/raw_data/raw_data1.csv").head(45000)
+data = pd.read_csv("./data/raw_data/raw_data_state.csv").head(45000)
 # 去掉功率为0的点
-data = data[data["active_power"] > 1]
-print(data.describe())
+data = data[data["active_power"] > 1][data["state"] == 6]
+# data =data[data["active_power"] > 1]
+print(data.groupby("state").count())
 # 切片:每隔5min取样
 data1 = data.iloc[0:35592:5]
 print(data1.head())
