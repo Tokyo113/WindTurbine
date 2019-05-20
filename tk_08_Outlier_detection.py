dc0962dadd5e45742be056c3f6447cfc5d39b9fc
Tokyo113 <21810179@zju.edu.cn>
2019-05-20 19:46:25 +0800

    k-means初步

--- tk_08_Outlier_detection.py
+++ tk_08_Outlier_detection.py
@@ -18,43 +18,50 @@ data1 = data1.drop("date", axis=1)
 
 # 参数初始化
 # 聚类个数
-k = 3
-# 离散点阈值
-threshold = 1.9
+k = 10
+# 离散点阈值   (3, 1.97)
+threshold = 2
 # 聚类最大循环次数
 iteration = 500
 # 数据标准化
 data_zs = 1.0*(data1 - data1.mean())/data1.std()
+print(data_zs.head(20))
 
-
+data_tk = data_zs[["wind_speed", "active_power"]]
+print(data_tk)
 # 聚类
 from sklearn.cluster import KMeans
 model = KMeans(n_clusters=k, max_iter=iteration)
-model.fit(data1)
+model.fit(data_tk)
 # print(pd.Series(model.labels_, index=data1.index))
 # 添加类别属性列
 cluster_data = pd.concat([data1, pd.Series(model.labels_, index=data1.index)], axis=1)
 cluster_data.columns = list(data1.columns) + ["category"]
+print(cluster_data.groupby("category").count())
 
 
 norm = []
 for i in range(k):
-    # norm_tmp = cluster_data[["Generator_speed", "Rotor_speed", "Gearbox_oil_temperature",
+    # norm_tmp = data_zs[["Generator_speed", "Rotor_speed", "Gearbox_oil_temperature",
     #              "Generator_bearing_temperature_drive", "Generator_bearing_temperature_nondrive",
     #              "wind_speed", "active_power"]][cluster_data["category"] == i]-model.cluster_centers_[i]
     # 只考虑风速和功率计算距离
-    norm_tmp = cluster_data[["wind_speed", "active_power"]][cluster_data["category"] == i] - model.cluster_centers_[i][5:]
+    norm_tmp = data_zs[["wind_speed", "active_power"]][cluster_data["category"] == i] - model.cluster_centers_[i]
     # 求绝对距离
     norm_tmp = norm_tmp.apply(np.linalg.norm, axis=1)
-    print(norm_tmp.median())
+
     # 求相对距离
     norm.append(norm_tmp/norm_tmp.median())
 
 
 
+
 norm = pd.concat(norm)
+
 cluster_data = pd.concat([cluster_data, norm], axis=1)
 cluster_data.columns = list(data1.columns) + ["category"] + ["distance"]
+# print(cluster_data.head(100))
+
 
 
 outier = cluster_data[cluster_data["distance"] >= threshold]
@@ -72,12 +79,23 @@ plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 # discrete_points = norm[norm > threshold]
 # discrete_points.plot(style='ro')
 # print(discrete_points)
+
+# 三个聚类中心点
+# plt.scatter(x=model.cluster_centers_[0][0], y=model.cluster_centers_[0][1], c='r', s=100, alpha=1)
+# plt.scatter(x=model.cluster_centers_[1][0], y=model.cluster_centers_[1][1], c='r', s=100, alpha=1)
+# plt.scatter(x=model.cluster_centers_[2][0], y=model.cluster_centers_[2][1], c='r', s=100, alpha=1)
+
+
+# 正常点与离群点
 plt.scatter(normal["wind_speed"], normal["active_power"], c='g', s=3, alpha=.5)
 plt.scatter(outier["wind_speed"], outier["active_power"], c='r', s=3, alpha=.5)
+
+
+# 三个类别示意图
 # plt.scatter(cluster1["wind_speed"], cluster1["active_power"], c='r', s=3, alpha=.5)
 # plt.scatter(cluster2["wind_speed"], cluster2["active_power"], c='b', s=3, alpha=.5)
 # plt.scatter(cluster3["wind_speed"], cluster3["active_power"], c='g', s=3, alpha=.5)
-# plt.show()
+plt.show()
 
 
 '''
@@ -90,7 +108,7 @@ plt.scatter(outier["wind_speed"], outier["active_power"], c='r', s=3, alpha=.5)
 
 
 思考:
-问题在于求相对距离的公式,最后除以中位数,中位数受极端值影响很大
+问题在于绘制风速-功率曲线用到的两个量风速和功率数量级相差太大,离群点受风速特征的影响体现不出来
 '''
 
 
