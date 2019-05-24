4a970028f63795d82e99e04a0d8e275c9b728237
Tokyo113 <21810179@zju.edu.cn>
2019-05-24 16:43:16 +0800

    5.24 Regression Analysis

--- tk_11_quartiles.py
+++ tk_11_quartiles.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 '''
 data: 5.23
 author: Tokyo
+desc:使用四分位法识别离群点,然后绘图显示
 '''
 
 
@@ -35,6 +36,10 @@ grouped = data["active_power"].groupby(quartiles).apply(get_status)
 
 # print(grouped.apply(get_status))
 print(type(grouped))
+
+
+
+# 四分位法处理数据
 s = []
 for a, b in data["active_power"].groupby(quartiles):
     label = []
@@ -54,6 +59,9 @@ for a, b in data["active_power"].groupby(quartiles):
 s = pd.concat(s)
 s = s.drop("active_power", axis=1)
 
+
+
+
 data = pd.concat([data1, s], axis=1)
 data.columns = list(data1.columns) + ["outlier"]
 print(data.groupby("outlier").count())
@@ -66,9 +74,16 @@ plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 normal = data[data["outlier"] == 0]
 outlier = data[data["outlier"] == 1]
 
+normal = normal.drop(["distance", "outlier", "category"], axis=1)
+
+# 生成csv文件
+# normal.to_csv('./data/data_preprocessing.csv', index=None)
+
+
+
 # 正常点与离群点
 plt.scatter(normal["wind_speed"], normal["active_power"], c='g', s=3, alpha=.5)
-plt.scatter(outlier["wind_speed"], outlier["active_power"], c='r', s=3, alpha=.5)
+# plt.scatter(outlier["wind_speed"], outlier["active_power"], c='r', s=3, alpha=.5)
 plt.show()
 
 
---
+++ tk_12_regression_analysis.py
@@ -0,0 +1,127 @@
+#coding:utf-8
+'''
+@Time: 2019/5/24 上午11:49
+@author: Tokyo
+@file: tk_12_regression_analysis.py
+@desc: 建立回归模型:线性回归,决策树,SVM,随机森林,AdaBoost,GBDT,XgBoost
+        而且做出图像对比预测值和实际值
+'''
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+def WT_preprocessing(gs=False, rs=False, dt=False, ndt=False, ws=False, ap=False):
+
+    # 1.读入数据
+    df = pd.read_csv("./data/data_preprocessing.csv")
+    # 得到标签(待预测值)
+    label = df["Gearbox_oil_temperature"]
+
+    df = df.drop("Gearbox_oil_temperature", axis=1)
+
+
+
+    # 特征处理
+    scaler_lst = [gs, rs, dt, ndt, ws, ap]
+    column_lst = ["Generator_speed", "Rotor_speed",
+                  "Generator_bearing_temperature_drive", "Generator_bearing_temperature_nondrive",
+                  "wind_speed", "wind_speed"]
+
+    for i in range(len(scaler_lst)):
+        if scaler_lst[i]:
+            df[column_lst[i]] = \
+                MinMaxScaler().fit_transform(df[column_lst[i]].values.reshape(-1, 1)).reshape(1, -1)[0]
+
+        else:
+            df[column_lst[i]] = \
+                StandardScaler().fit_transform(df[column_lst[i]].values.reshape(-1, 1)).reshape(1, -1)[0]
+
+    return df, label
+
+
+def WT_modeling(features, label):
+    from sklearn.model_selection import train_test_split
+    f_v = pd.DataFrame(features).values
+    f_names = pd.DataFrame(features).columns.values
+    l_v = pd.DataFrame(label).values
+
+    # 切分训练集,测试集,验证集
+    X_tt, X_validation, Y_tt, Y_validation = train_test_split(f_v, l_v, test_size=0.2)
+    X_train, X_test, Y_train, Y_test = train_test_split(X_tt, Y_tt, test_size=0.25)
+
+    from sklearn.linear_model import LinearRegression, Ridge, Lasso
+    from sklearn.metrics import mean_squared_error, mean_absolute_error, median_absolute_error
+    from sklearn.tree import DecisionTreeRegressor
+    from sklearn.svm import SVR
+    from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor, GradientBoostingRegressor
+    from xgboost import XGBRegressor
+    models = []
+    # 线性回归
+    # models.append(("LinearRegression", LinearRegression()))
+    # models.append(("Ridge", Ridge(alpha=0.6)))
+    # models.append(("Lasso", Lasso(alpha=0.002)))
+    # 决策树回归
+    models.append(("DecisionTreeRegressor", DecisionTreeRegressor()))
+    # 支持向量回归(误差很大)
+    # models.append(("SVR", SVR(C=100000)))
+    models.append(("RandomForestRegressor", RandomForestRegressor()))
+    # AdaBoostRegressor  base_estimator=DecisionTreeRegressor默认
+    models.append(("AdaBoostRegressor", AdaBoostRegressor()))
+    # GBDT 回归
+    models.append(("GradientBoostingRegressor", GradientBoostingRegressor()))
+    # XGBoost
+    models.append(("XGBoost", XGBRegressor()))
+
+
+
+    for regr_name, regr in models:
+        regr.fit(X_train, Y_train)
+        xy_lst = [(X_train, Y_train), (X_validation, Y_validation), (X_test, Y_test)]
+        for i in range(len(xy_lst)):
+            X_part = xy_lst[i][0]
+            Y_part = xy_lst[i][1]
+            Y_pred = regr.predict(X_part)
+
+            print(i)
+            # 0--训练集, 1--验证集, 2--测试集
+            print(regr_name, "mean_squared_error", mean_squared_error(Y_part, Y_pred))
+            print(regr_name, "mean_absolute_error", mean_absolute_error(Y_part, Y_pred))
+            print(regr_name, "median_absolute_error", median_absolute_error(Y_part, Y_pred))
+
+
+
+def WT_figure(features, label):
+    from sklearn.model_selection import train_test_split
+    from xgboost import XGBRegressor
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    f_v = pd.DataFrame(features).values
+    f_names = pd.DataFrame(features).columns.values
+    l_v = pd.DataFrame(label).values
+
+    # 切分训练集,测试集,验证集
+    X_tt, X_validation, Y_tt, Y_validation = train_test_split(f_v, l_v, test_size=0.2)
+    X_train, X_test, Y_train, Y_test = train_test_split(X_tt, Y_tt, test_size=0.25)
+
+    xgb = XGBRegressor().fit(X_train, Y_train)
+    Y_pred = xgb.predict(X_test)
+    print(Y_pred)
+    y_pre = pd.Series(Y_pred)
+    y_pre[150:250].plot(c='g')
+    y_test = Y_test.flatten()
+    y_test = pd.Series(y_test)
+    y_test[150:250].plot(c='y')
+    plt.show()
+
+
+
+
+def main():
+    features, label = WT_preprocessing()
+    WT_modeling(features, label)
+    WT_figure(features, label)
+
+
+if __name__ == '__main__':
+    main()
