ae4319716bfde4a080cae857b4c61106ea02a6ae
Tokyo113 <21810179@zju.edu.cn>
2019-06-19 16:58:20 +0800

    6.9feature selection

--- tk_18_data_preprocessing.py
+++ tk_18_data_preprocessing.py
@@ -27,7 +27,7 @@ def wt_draw_scatter(data, x, y):
 
 
 def wt_preprocessing(data, method):
-    from sklearn.preprocessing import MinMaxScaler,StandardScaler
+    from sklearn.preprocessing import MinMaxScaler, StandardScaler
     data = data.drop("date", axis=1)
     data = data.drop("state", axis=1)
 
@@ -95,20 +95,22 @@ def feature_tree(features, label, names):
 
 def main():
 
-    df = pd.read_csv('./data/feature2018_33_2.csv')
-    print(df.count())
-    # print(df.groupby("state").count())
-    # 切片:每隔5min取样
-    df = df.iloc[129550:435304:5]
-    df = df[df["Active_power"] > 1][df["state"] == 6]
-    df = df[df["Wind_speed"] <= 18]
-    df.rename(columns={'Active_power': 'active_power', 'Wind_speed': 'wind_speed'}, inplace=True)
-    df = df.drop_duplicates(subset=['date'])
-    df_1 = DBSCAN_cluster(df, 0.1, 25)
-    df_2 = Quartiles(df_1, 1.5, 50)
+    # df = pd.read_csv('./data/feature2018_33_2.csv')
+    # print(df.count())
+    # # print(df.groupby("state").count())
+    # # 切片:每隔5min取样
+    # df = df.iloc[129550:435304:5]
+    # df = df[df["Active_power"] > 1][df["state"] == 6]
+    # df = df[df["Wind_speed"] <= 18]
+    # df.rename(columns={'Active_power': 'active_power', 'Wind_speed': 'wind_speed'}, inplace=True)
+    # df = df.drop_duplicates(subset=['date'])
+    # df_1 = DBSCAN_cluster(df, 0.1, 25)
+    # df_2 = Quartiles(df_1, 1.5, 50)
 
     # 2018年4月测试集
-    df_2.to_csv('./data/data2018_single_month_test.csv', index=None)
+    # df_2.to_csv('./data/data2018_single_month_test.csv', index=None)
+
+
     # 数据预处理
     # df = pd.read_csv('./data/year/feature2018_33.csv').head(45000)
     # # 去掉功率为0的点
@@ -128,14 +130,14 @@ def main():
 
 
     # 特征选择
-    # data2018 = './data/data2018_single_month_33.csv'
-    # data = pd.read_csv(data2018)
-    # # wt_draw_scatter(data, 'Gearbox_oil_temp', 'active_power')
-    # features, label, names = wt_preprocessing(data, False)
+    data2018 = './data/data2018_single_month_33.csv'
+    data = pd.read_csv(data2018)
+    # wt_draw_scatter(data, 'Gearbox_oil_temp', 'active_power')
+    features, label, names = wt_preprocessing(data, False)
     # feature_RFE(features, label, names)
     # feature_filter(features, label, names)
     # feature_tree(features, label, names)
-    # wt_params(features, label)
+    wt_params(features, label)
 
 
 
@@ -153,6 +155,25 @@ if __name__ == '__main__':
 引入前两个时刻的温度值后效果很明显,
 接下来:
 尝试和对比模型融合方法: 线性回归+RF+XGB
+
+
+选择出的特征:
+RFE方法:
+Avg_Wind_speed_3s, Avg_active_pow_300s, Avg_active_power_30s, Avg_wind_speed_30s, Avg_wind_speed_5min,Consumption_active
+Consumption_reactive, Generation_reactive, Generation_reactive, Grid_frequency, PCS_rotor_temp, PCS_speed,PCS_torque
+Rotor_speed,active_power, temp_1, temp_2, voltage_phaseA, voltage_phaseB, voltage_phaseC
+
+过滤法:
+temp_1, temp_2,Avg_wind_speed_30s,Avg_active_pow_300s,Avg_wind_speed_5min,Avg_active_power_30s
+Avg_Wind_speed_3s,active_power, Grid_ap,PCS_speed,Rotor_speed, Generator_speed, wind_speed,
+PCS_torque,Generator_bearing_tem_nondrive
+ 
+ 
+ 
+目前暂定选择:
+temp_1, temp_2, Avg_active_pow_300s, PCS_speed,Avg_wind_speed_30s,Generator_bearing_tem_nondrive,
+active_power,Generator_speed,PCS_torque,Rotor_speed,Generator_bearing_tem_drive,PCS_rotor_temp,Avg_wind_speed_5min
+Pitch_angle,Avg_active_power_30s,Generation_reactive,
 
 
 '''
---
+++ tk_19_feature_selection.py
@@ -0,0 +1,35 @@
+#coding:utf-8
+'''
+@Time: 2019/6/19 下午3:52
+@author: Tokyo
+@file: tk_19_feature_selection.py
+@desc:
+'''
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from tk_18_data_preprocessing import wt_preprocessing
+from tk_14_single_year import wt_params
+
+def feature_selection(df):
+
+    df = df.drop(['Avg_pitch_angle', 'Grid_frequency', 'Power_factor', 'Grid_ap',
+                  'Nacelle_revolution', 'voltage_phaseA', 'voltage_phaseB'], axis=1)
+    print(df.describe())
+    return df
+
+
+
+
+def main():
+    df = pd.read_csv('./data/data2018_single_month_33.csv')
+    df1 = feature_selection(df)
+    features, label, names = wt_preprocessing(df1, False)
+    wt_params(features, label)
+
+
+
+
+if __name__ == '__main__':
+    main()
