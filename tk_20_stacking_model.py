7abebdf24f961902e41a8f3ceaa141134be1c3b0
Tokyo113 <21810179@zju.edu.cn>
2019-07-04 12:05:01 +0800

    7.4 stacking

--- tk_20_stacking_model.py
+++ tk_20_stacking_model.py
@@ -10,16 +10,17 @@
 def stacking_model(features, label):
     from sklearn.model_selection import train_test_split
     from xgboost import XGBRegressor
-    from sklearn.ensemble import RandomForestRegressor
+    from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
     from sklearn.linear_model import LogisticRegression, Ridge
     from mlxtend.regressor import StackingCVRegressor
     from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
     X_train, X_validation, Y_train, Y_validation = train_test_split(features, label, test_size=0.2, shuffle=None)
     xgb = XGBRegressor()
     rfr = RandomForestRegressor()
+    gbdt = GradientBoostingRegressor()
     lr = Ridge()
 
-    srgr = StackingCVRegressor(regressors=[xgb, rfr], meta_regressor=lr)
+    srgr = StackingCVRegressor(regressors=[xgb, rfr, gbdt], meta_regressor=xgb, cv=5)
 
     srgr.fit(X_train, Y_train)
     Y_pred = srgr.predict(X_validation)
