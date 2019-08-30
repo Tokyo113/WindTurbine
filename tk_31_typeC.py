7dd3f494c6f2107fd7430e33768aab8a3e4eadba
Tokyo113 <21810179@zju.edu.cn>
2019-08-30 15:22:49 +0800

    8.30

--- tk_31_typeC.py
+++ tk_31_typeC.py
@@ -1,9 +1,9 @@
 #coding:utf-8
 '''
-@Time: 2019/8/13 上午10:29
+@Time: 2019/8/30 下午2:01
 @author: Tokyo
-@file: tk_31_typeB.py
-@desc:
+@file: tk_31_typeC.py
+@desc: 一台风机验证
 '''
 
 
@@ -19,14 +19,14 @@ from tk_27_preprocessing import wt_preprocessing
 from tk_21_test_model57 import stacking_MD
 from tk_tools import WT_modeling2
 
-df1 = pd.read_csv('./data/B/train/training set.csv')
+df1 = pd.read_csv('./data/C/train/training set.csv')
 df1 = df1.dropna()
 
 df1 = feature_selection(df1)
 
 # stacking_model(features, label)
 
-df1_t = pd.read_csv('./data/B/hfj149_83/test data.csv')
+df1_t = pd.read_csv('./data/C/hfj036_3/validation set.csv')
 df1_t = df1_t.dropna()
 df1_t = feature_selection(df1_t)
 
@@ -47,42 +47,39 @@ s = pd.Series(s)
 
 # 作图
 f = plt.figure(figsize=(10, 10))
-
 ax1 = f.add_subplot(3, 1, 1)
 y_pred1 = pd.Series(y_pred1)
 y_pred1.plot(c='g', label='Predict')
 y_test1 = y_test1.reset_index(drop=True)
-# # flatten 降维
-y_test1.plot(c='y', label='Test')
-x_ticks = ax1.set_xticks([0, 1000, 2000, 3000, 4000, 5000])
-x_labels = ax1.set_xticklabels(["Dec/01", "Dec/06", "Dec/11", "Dec/17", "Dec/22",  "Dec/29"],
+# flatten 降维
+y_test1.plot(c='y', label='Measured value')
+x_ticks = ax1.set_xticks([0, 1000, 2000, 3000, 4000, 5000, 6000])
+x_labels = ax1.set_xticklabels(["July/01", "July/06", "July/11", "July/15", "July/20", "July/25",  "July/29"],
                                    rotation=30, fontsize="small")
 plt.ylabel('Oil Temperature/(Deg.C)')
-plt.legend(loc='lower left', ncol=2)
-
+plt.legend(loc='upper left', ncol=2)
 
 
 
 ax2 = f.add_subplot(3, 1, 2)
 md1.plot()
-x_ticks = ax2.set_xticks([0, 1000, 2000, 3000, 4000, 5000])
-x_labels = ax2.set_xticklabels(["Dec/01", "Dec/06", "Dec/11", "Dec/17", "Dec/22",  "Dec/29"],
-                                   rotation=30, fontsize="small")
 plt.ylim((0, 20))
 y_ticks = ax2.set_yticks([0, 5, 10, 15, 20])
+x_ticks = ax2.set_xticks([0, 1000, 2000, 3000, 4000, 5000, 6000])
+x_labels = ax2.set_xticklabels(["July/01", "July/06", "July/11", "July/15", "July/20", "July/25",  "July/29"],
+                               rotation=30, fontsize="small")
 plt.ylabel('Mahalanobis Distance')
 
 
 
 ax3 = f.add_subplot(3, 1, 3)
 s.plot()
-x_ticks = ax3.set_xticks([0, 1000, 2000, 3000, 4000, 5000])
-x_labels = ax3.set_xticklabels(["Dec/01", "Dec/06", "Dec/11", "Dec/17", "Dec/22",  "Dec/29"],
-                                   rotation=30, fontsize="small")
+plt.ylim((-1500, 0))
+x_ticks = ax3.set_xticks([0, 1000, 2000, 3000, 4000, 5000, 6000])
+x_labels = ax3.set_xticklabels(["July/01", "July/06", "July/11", "July/15", "July/20", "July/25",  "July/29"],
+                               rotation=30, fontsize="small")
 
-plt.ylim((-1300, 200))
 plt.ylabel('CUSUM chart')
 plt.xlabel('Date')
-plt.savefig('./data/figure/test normalB.png', dpi=300)
-plt.show()
-
+plt.savefig('./data/figure/test normalA.png', dpi=300)
+plt.show() 
